500d1a62a58eb28601ffe5fdddc55fd1b56ae5b8
AzatNavatov <anavatov4@gmail.com>
2024-06-18 11:52:57 +0600

    Version 1

---
+++ SEBeginner4/Program.cs
@@ -0,0 +1,123 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] myItems = new string[] { "Laptop", "bottle", "mouse", "backpack" };
+        Console.WriteLine(myItems[1]);
+
+        char[] someChars = new char[33];
+
+        Console.WriteLine(someChars[0]);
+
+        int[] digits;
+
+        digits = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Console.WriteLine(digits[7]);
+        Console.WriteLine(myItems.Length);
+
+        string joinedMyItems = string.Join("+", myItems);
+        Console.WriteLine($"{joinedMyItems}");
+        Console.WriteLine($"{{joinedMyItems}}");
+        Console.WriteLine($"{{{joinedMyItems}}}");
+
+        string sentence = "my items are: ";
+
+        for (int i = 0; i < myItems.Length; i++)
+        {
+            sentence += myItems[i];
+            if ((i + 1) < myItems.Length)
+            {
+                sentence = sentence + ",";
+            }
+        }
+        Console.WriteLine(sentence);
+
+
+        int irCounter = 0;
+        foreach (string item in myItems)
+        {
+            if (irCounter == 0)
+            {
+                Console.WriteLine("This is my: " + item + " and");
+            }
+            else if (irCounter + 1 < myItems.Length)
+            {
+                Console.WriteLine("this is my: " + item + " and");
+            }
+
+            else
+            {
+                Console.WriteLine("this is my: " + item + ".");
+            }
+            irCounter++;
+        }
+
+        string myName = "Navatov Azat";
+
+        string myInitials = myName.Substring(0,1) +"."+ myName.Substring(8,1)+".";
+        Console.WriteLine(myInitials);
+
+        Console.Clear();
+
+        Random myRand = new Random();
+        int[] my11 = new int[11];
+
+        string frArray = string.Join("\t", my11);
+        Console.WriteLine("my list without adding values: " + Environment.NewLine + frArray);
+
+        for (int i = 0;i < my11.Length;i++) 
+        {
+            my11[i] = myRand.Next(8000,10000);
+        }
+
+        frArray = string.Join("\t", my11);
+        Console.WriteLine("my list after adding values: " + Environment.NewLine + frArray);
+
+        frArray = "";
+
+        for (int i = 0; i < my11.Length; i++)
+        {
+            frArray += my11[i].ToString("N0");
+            if((i)<my11.Length) 
+            { 
+                frArray = frArray + "\t";
+            }
+        }
+
+        Console.WriteLine("my list after formatting woth 1000 point:" + Environment.NewLine + frArray);
+
+        Array.Sort(my11);
+
+        frArray = string.Join("\t", my11.Select(a=>a.ToString("N0")));
+
+        Console.WriteLine("After asc sorting and 1000 formattong: " + Environment.NewLine + frArray);
+
+        Array.Reverse(my11);
+
+        frArray = string.Join("\t", my11.Select(a => a.ToString("N0")));
+
+        Console.WriteLine("After desc sorting and 1000 formattong: " + Environment.NewLine + frArray);
+
+        my11[9] = 12500;
+        frArray = string.Join("\t", my11.Select(a => a.ToString("N0")));
+
+        Console.WriteLine("After adding new number: " + Environment.NewLine + frArray);
+
+     
+
+        int[] my12 = new int[12];
+        my11.CopyTo(my12, 0);
+
+        my12[10] = 12000;
+
+        frArray = string.Join("\t", my12.Select(a => a.ToString("N0")));
+        Console.WriteLine("After adding new number after copying the previous array to new one: " + Environment.NewLine + frArray);
+
+
+
+
+
+
+
+    }
+}
---
+++ SEBeginner5/Program.cs
@@ -0,0 +1,87 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //FirstPart();
+        int[][] JgArray = new int[2][];
+
+        Random myRand = new Random();
+
+        for (int i = 0; i < JgArray.GetLength(0); i++)
+        {
+            JgArray[i] = new int[myRand.Next(80, 92)];
+        }
+
+
+        List<int> myList = new List<int>();
+        myList.Add(25);
+        myList.Add(30);
+        myList.Add(50);
+        myList.Add(35);
+
+        myList.Remove(35);
+
+        myList.RemoveAt(myList.Count - 1);
+        Console.WriteLine(string.Join(";", myList));
+
+        int[] myListArray = myList.ToArray();
+        myList.AddRange(new List<int> { 88, 99 });
+
+        bool Contain88 = myList.Contains(88);
+        bool Contain2000 = myList.Contains(200);
+        Console.WriteLine($"Our list contains contains 88:" + Contain88 +
+            Environment.NewLine + "Our list contains 2000:" + Contain2000);
+
+        int[] myArray = new int[] { 10, 20, 30, 40, 55, 20 };
+
+        myList.CopyTo(myArray, 1);
+
+        Console.WriteLine(string.Join(",", myArray));
+        Console.WriteLine(string.Join(",", myList));
+
+        myList.RemoveAll(x => x > 50);
+        Console.WriteLine(string.Join(",", myList));
+
+        bool Contain25 = myList.Exists(a => a == 25);
+        bool Contain1000 = myList.Exists(a => a == 1000);
+        Console.WriteLine($"{Contain25} + {Contain1000}");
+
+
+        int[] myArrayOp = new int[] { 2, 5, 10 };
+
+        if (myArrayOp.Length > 4 & myArrayOp[1] == 12)
+        {
+            Console.WriteLine("&");
+        }
+
+        if (myArrayOp.Length > 4 && myArrayOp[1] == 12)
+        {
+            Console.WriteLine("&&");
+        }
+
+
+    }
+    static void FirstPart()
+    {
+        int[] oneDimArray = new int[] { 10, 20, 30, 40, 55 };
+
+        oneDimArray[1] = 25;
+
+        int[] tempOneDimArray = new int[oneDimArray.Length];
+        Array.Copy(oneDimArray, tempOneDimArray, oneDimArray.Length);
+
+        oneDimArray = new int[10];
+
+        Array.Copy(tempOneDimArray, oneDimArray, tempOneDimArray.Length);
+
+        int[,,,,] Dim5Array = new int[2, 5, 10, 20, 2];
+
+
+        Console.WriteLine($"Length of our 5D Array {Dim5Array.Length.ToString("N0")}");
+
+
+        Console.WriteLine($"Length of our 5D Array {Dim5Array.GetLength(0).ToString("N0")}");
+
+
+    }
+}
